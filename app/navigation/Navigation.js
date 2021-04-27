b58e06c1175dcc128472f78dcd25a8b94bd92503
Sebastian Hernandez Caro <sebastian.hernandez@PSOFKA0318.local>
2021-04-27 16:53:05 -0500

    feature: icons and color was add to the navbar

--- app/navigation/Navigation.js
+++ app/navigation/Navigation.js
@@ -1,21 +1,63 @@
 import React from "react";
-import {NavigationContainer} from "@react-navigation/native";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "react-native-elements";
 import PlacesStack from "../navigation/PlacesStack"
 import FavoritesStack from "../navigation/FavoritesStack";
 import TopPlacesStack from "../navigation/TopplacesStack";
 import SearchStack from "../navigation/SearchStack";
 import AccountSatck from "../navigation/AccountStack";
 
-
 const Tab = createBottomTabNavigator();
+
+
+function screenOptions(route, color) {
+    let iconName;
+
+    switch(route.name) {
+        case "pet-places" :
+            iconName = "city"
+            break;
+        case "favorites" :
+            iconName = "paw"
+            break;
+        case "top-places" :
+            iconName = "bone"
+            break;
+        case "search" :
+            iconName = "map-search-outline"
+            break;
+        case "account" :
+            iconName = "dog"
+            break;
+        default:
+            break;    
+    }
+    return(
+        <Icon 
+           type = "material-community"
+           name = {iconName}
+           size = {22}
+           color = {color}
+         />  
+
+    )
+}
 
 
 export default function Navigation(){
     return(
         <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+            initialRouteName = "pet-places"
+            tabBarOptions = {{
+                inactiveTintColor: "#646464",
+                activeTintColor: "#19AFFF"
+            }}
+            screenOptions = { ({ route }) => ({
+                tabBarIcon: ({color}) =>  screenOptions(route, color)
+            })}
+        >
             <Tab.Screen 
                 name = "pet-places" 
                 component = {PlacesStack}
