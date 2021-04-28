2b7bc811c17362397b3d05406c3a66b837c7101f
Sebastian Hernandez Caro <sebastian.hernandez@PSOFKA0318.local>
2021-04-28 11:31:36 -0500

    feature: Loading spiner component was create and  add to Account Screen

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,1 +1,47 @@
+import React from "react";
+import { StyleSheet, View, Text, ActivityIndicator } from "react-native";
+import { Overlay } from "react-native-elements";
 
+export default function Loading( props ) {
+   const { isVisible, text } = props;
+
+   return(
+       <Overlay 
+            isVisible={isVisible}
+            windowBackgroundColor="rgba(0, 0, 0, 0.5)"
+            overlayBackgroundColor="trasnparent"
+            overlayStyle={styles.overlay}              
+       >
+           <View style={styles.view}>
+               <ActivityIndicator 
+                    size="large"
+                    color="#19AFFF"
+                />    
+                {text && <Text style={styles.text}>{text}</Text>} 
+           </View>
+       </Overlay>
+   )
+}
+
+
+const styles = StyleSheet.create({
+    overlay: {
+        height: 100,
+        width: 200,
+        backgroundColor: "#fff",
+        borderColor: "#19AFFF",
+        borderWidth: 2,
+        borderRadius: 10,
+    },
+    view : {
+        flex:1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        color: "#19AFFF",
+        textTransform: "uppercase",
+        marginTop: 10,
+    },
+});
+ 
--- app/screens/Account/Account.js
+++ app/screens/Account/Account.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from "react"
-import {View, Text} from "react-native";
 import * as firebase from "firebase";
 import UserGuest from "./UserGuest";
 import UserLogged from "./UserLogged";
+import Loading from "../../components/Loading"
 
 export default function Account() {
    
@@ -16,7 +16,7 @@ export default function Account() {
     }, []);
 
 
-    if (login === null) return <Text>Cargando..</Text>
+    if (login === null) return <Loading isVisible={true} text="cargando" />
 
     return login ? <UserLogged /> : <UserGuest />
 }
