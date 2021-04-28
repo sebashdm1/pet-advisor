423f217ccac96837b6d62b6351b7a6c9d9a59f6b
Sebastian Hernandez Caro <sebastian.hernandez@PSOFKA0318.local>
2021-04-28 15:13:35 -0500

    feature: Image and button is created to the user guest screen

--- app/screens/Account/UserGuest.js
+++ app/screens/Account/UserGuest.js
@@ -1,10 +1,46 @@
 import React from "react"
-import {View, Text} from "react-native";
+import { StyleSheet, ScrollView, View, Image } from "react-native";
+import { Button} from "react-native-elements"
 
 export default function userGuest() {
    return(
-       <View>
-           <Text>user guest.</Text>
-       </View>
-   )
+       <ScrollView centerContent={true} style={styles.viewBody}>
+           <Image
+               source={require("../../../assets/img/user-guest.png")}
+               resizeMede="contain"
+               style={styles.image}
+           /> 
+           <View style={styles.viewBtn}>
+            <Button 
+                title="Ingresar"
+                buttonStyle={styles.btnStyle}
+                containerStyle={styles.btnContainer} 
+                onPress={() => console.log("clivk")}
+            />
+          </View>   
+       </ScrollView>
+   );
 }
+
+const styles = StyleSheet.create({
+  viewBody: {
+      backgroundColor: "#070707"
+  },
+  image: {
+      height: 400,
+      width: "100%",
+      marginBottom: 40,
+  },
+  viewBtn:{
+      flex: 1,
+      alignItems: "center"
+  },
+  btnContainer: {
+      width: "50%",
+      marginTop: 20,
+  },
+  btnStyle: {
+      backgroundColor: "#19AFFF"
+  }
+
+})
