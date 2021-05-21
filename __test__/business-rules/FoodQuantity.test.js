3a6b99a7dc35419c34c73a6e4c65aada981ac1da
Sebastian Hernandez <sebashdm@gmail.com>
2021-05-21 00:55:35 -0500

    regla de negocio para cantidad de alimento, se crean test y regla de negocio para determinar la cantidad de alimento que debe consumir un perro segun su edad

--- __test__/business-rules/FoodQuantity.test.js
+++ __test__/business-rules/FoodQuantity.test.js
@@ -1,3 +1,21 @@
-import {QuantityFoodToDog} from '../../__test__/business-rules/FoodQuantity.test'
+import {QuantityFoodToDog} from '../../app/business-rules/FoodQuantity'
 
-describe('testing q', () => {})
+describe('amount of food that dog should eat', () => {
+  test('if dog weigth is 10kg he should eat 250g of food  ', () => {
+    let result = QuantityFoodToDog(10)
+    expect(result).toBe(250)
+  })
+  test('if dog weigth is 20kg he should eat 400g of food  ', () => {
+    let result = QuantityFoodToDog(20)
+    expect(result).toBe(400)
+  })
+  test('if dog weigth is 60kg he should eat 850g of food  ', () => {
+    let result = QuantityFoodToDog(60)
+    expect(result).toBe(850)
+  })
+
+  test('if dog weigth is 50kg he shouldnt eat 850g of food  ', () => {
+    let result = QuantityFoodToDog(50)
+    expect(result).not.toBe(850)
+  })
+})
